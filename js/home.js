5dd90adf3d9577db7910c1accf281d973dd7af84
alvarot2601 <alvarot2601@gmail.com>
2022-12-02 20:12:26 +0100

    maqueto la pagina room-details

--- js/home.js
+++ js/home.js
@@ -2,21 +2,72 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-  
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
     },
+    //autoHeight:true,
+    
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    }
+  });
+  const swiperFoodMenu = new Swiper('.swiper--food-menu', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
   
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
+    }
+  });
+  const swiperRoom = new Swiper('.swiper--room', {
+    direction: 'horizontal',
+      loop: true,
+      // If we need pagination
+      //autoHeight:true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      }
+  });
+   
+  const swiperRooms = new Swiper('.roomsSwiper', {
+    autoHeight:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + "</span>";
+      },
     },
+  });
+ const swiperFood = new Swiper('.swiper--food', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
   
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
     // And if we need scrollbar
     scrollbar: {
       el: '.swiper-scrollbar',
+    },
+  });
+  
+  var swiper2 = new Swiper(".mySwiper", {
+    autoHeight:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + "</span>";
+      },
     },
   });
 
